d8a072ca7c971cdd51449ebfaedc2a0766f119f6
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 15:53:06 +0100

    get all users function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,3 +9,19 @@ const usersController = {
       .catch((err) => res.status(400).json(err));
   },
 
+  // Get All Users
+  getAllUsers(req, res) {
+    Users.find({})
+      // populate users thoughts
+      .populate({ path: "thoughts", select: "-__v" })
+      // populate user friends
+      .populate({ path: "friends", select: "-__v" })
+      .select("-__v")
+      .then((dbUsersData) => res.json(dbUsersData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Get a single user by ID
