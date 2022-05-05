860c0412bc5caa2facfe31c32e18dddbfb1408b9
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 15:52:50 +0100

    create a new User function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,11 @@
 // Requiring Users Model
 const { Users } = require("../models");
+
+const usersController = {
+  // Create a new User
+  createUsers({ body }, res) {
+    Users.create(body)
+      .then((dbUsersData) => res.json(dbUsersData))
+      .catch((err) => res.status(400).json(err));
+  },
+
