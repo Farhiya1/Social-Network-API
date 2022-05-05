8888b56301023678ca963fb656cb712df69335de
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 15:38:55 +0100

    edited server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -8,15 +9,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Connecting to routes
 app.use(require("./routes"));
 
+// Connecting to database
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
-  {}
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 mongoose.set("debug", true);
 
+// Connecting to server
 app.listen(PORT, () =>
   console.log(`😊😊😊 You are connected on localhost:${PORT}😊😊😊`)
 );
