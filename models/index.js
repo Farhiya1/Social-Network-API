030d9921a03c362a292366867ffb3004d558bdc0
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 15:59:02 +0100

    exporting models for user and thoughts

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Requiring Users and Thoughts Models
+const Thoughts = require("./Thought");
+const Users = require("./User");
+
+// Exporting  Users and Thoughts Model.
+module.exports = { Users, Thoughts };
