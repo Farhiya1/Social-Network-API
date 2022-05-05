8abbba0e84b868197e70d368ffc545d2c1ae547e
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-05-05 16:01:08 +0100

    schema for reaction

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+// Requiring Mongoos and Moment.js
+const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
+
+// Schema for Reactions
+const ReactionsSchema = new Schema(
+  {
+    // Setting up custom ID
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
